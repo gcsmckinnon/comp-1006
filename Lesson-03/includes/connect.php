2f9dc77e37b7e624f7e532919423445225512937
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2019-09-15 10:43:28 -0400

    init for lesson 3

---
+++ Lesson-03/includes/connect.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+        A simple reusable connection script
+        https://www.php.net/manual/en/pdo.construct.php
+    */
+
+    // Our connection details
+
+    // Creates the connection
+    try {
+    } catch (PDOException $e) {
+    }
